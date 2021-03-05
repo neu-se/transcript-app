82497d0c0940ac2f1c0d3b218c0198982c7476e0
Xin Guan <imbaguanxin@outlook.com>
2021-03-05 14:44:36 -0500

    update issue 6

--- src/server/server.test.ts
+++ src/server/server.test.ts
@@ -103,6 +103,6 @@ describe('TranscriptREST', () => {
   });
 
   describe('issue #1', () => {
-
+    
   });
 });
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,12 +1,11 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 // app-specific packages
-import { transcriptManager as db } from '../types/local-transcript-manager';
+import { transcriptManager as db } from "../types/local-transcript-manager";
 
 // create the server, call it app
 export const app: express.Application = express();
-
 
 // allow requests from any port or source.
 app.use(cors());
@@ -20,15 +19,15 @@ app.use(express.json());
 // GET/ : just counts the number of GET 'requests' we've had
 // so far. Just for heartbeat checking.
 let ngets = 0;
-app.get('/', (req: express.Request, res: express.Response) => {
+app.get("/", (req: express.Request, res: express.Response) => {
   ngets++;
-  console.log('Handling GET/', ngets);
+  console.log("Handling GET/", ngets);
   res.status(200).send(`This is GET number ${ngets} on the current server`);
 });
 
 // GET /transcripts
-app.get('/transcripts', (req, res) => {
-  console.log('Handling GET/transcripts');
+app.get("/transcripts", (req, res) => {
+  console.log("Handling GET/transcripts");
   const data = db.getAll();
   console.log(data);
   res.status(200).send(data);
@@ -39,11 +38,11 @@ app.get('/transcripts', (req, res) => {
 // returns an ID for this student.
 // Requires a post parameter 'name'
 
-app.post('/transcripts', (req, res) => {
+app.post("/transcripts", (req, res) => {
   // use req.body.name to get the value of the post parameter (in the body)
   // don't know what happens if the info is missing.  We should fail gracefully
   const studentName: string = req.body.name;
-  if(!studentName) {
+  if (!studentName) {
     res.status(400).send(`No student name specified`);
     return;
   }
@@ -51,7 +50,7 @@ app.post('/transcripts', (req, res) => {
   console.log(`Handling POST/transcripts name=${studentName}, id=${studentID}`);
   // sending a number makes the server think the number
   // is the response status.
-  res.status(200).send({studentID});
+  res.status(200).send({ studentID });
 });
 
 // GET  /transcripts/:id           --
@@ -59,9 +58,9 @@ app.post('/transcripts', (req, res) => {
 // Fails with a 404 if no such student
 // req.params will look like {"id": 301}
 
-app.get('/transcripts/:id', (req, res) => {
+app.get("/transcripts/:id", (req, res) => {
   // req.params to get components of the path
-  const {id} = req.params;
+  const { id } = req.params;
   console.log(`Handling GET /transcripts/:id id = ${id}`);
   const theTranscript = db.getTranscript(parseInt(id));
   if (theTranscript === undefined) {
@@ -75,8 +74,8 @@ app.get('/transcripts/:id', (req, res) => {
 // returns list of IDs for student with the given name
 // returns empty list if none
 
-app.get('/studentids', (req, res) => {
-// use req.query to get value of the parameter
+app.get("/studentids", (req, res) => {
+  // use req.query to get value of the parameter
   const studentName = req.query.name as string;
   console.log(`Handling GET /studentids studentName=${studentName}`);
   const ids = db.getStudentIDs(studentName);
@@ -90,17 +89,20 @@ app.get('/studentids', (req, res) => {
 // Fails with 400 (Bad Request) if there is already an entry for this course
 //  in the student's transcript
 
-app.post('/transcripts/:studentID/:course', (req, res) => {
+app.post("/transcripts/:studentID/:course", (req, res) => {
   try {
     const studentID = parseInt(req.params.studentID);
     const course = req.params.course as string;
     const grade = parseInt(req.body.grade as string);
-    if(!grade) {
-      res.status(400).send(`Invalid grade, must be a number. Got ${req.body.grade}`);
+    if (!grade) {
+      res
+        .status(400)
+        .send(`Invalid grade, must be a number. Got ${req.body.grade}`);
       return;
     }
-    console.log
-    (`Handling POST studentid = ${studentID}; course = ${course}, grade = ${grade}`);
+    console.log(
+      `Handling POST studentid = ${studentID}; course = ${course}, grade = ${grade}`
+    );
     db.addGrade(studentID, course, grade);
     res.sendStatus(200);
   } catch (e) {
@@ -113,18 +115,49 @@ app.post('/transcripts/:studentID/:course', (req, res) => {
 // returns the student's grade in the specified course.
 // Fails if student or course is missing.
 // uses function getGrade(studentID:StudentID, course:Course) : number
-app.get('/transcripts/:studentID/:course', (req, res) => {
+app.get("/transcripts/:studentID/:course", (req, res) => {
   try {
     const studentID = parseInt(req.params.studentID);
-    const {course} = req.params;
+    const { course } = req.params;
     console.log(`Handling GET studentID=${studentID} course = ${course}`);
     const grade = db.getGrade(studentID, course);
     if (grade == undefined) {
-      res.status(400).send(`not grade for ${studentID} in ${course}`)
+      res.status(400).send(`not grade for ${studentID} in ${course}`);
     }
-    res
-      .status(200)
-      .send({studentID, course, grade});
+    res.status(200).send({ studentID, course, grade });
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+app.get("/courses", (req, res) => {
+  try {
+    console.log("getting courses list with discription");
+    const courses = db.getCourseDescriptions();
+    res.status(200).send(courses);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+app.post("/courses", (req, res) => {
+  try {
+    console.log("posting a new course");
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+app.get("/courses/:course", (req, res) => {
+  try {
+    console.log("getting a course with name");
+    const courseName = req.params.course
+    const course = db.getCourseDescription(courseName)
+    if (course === undefined) {
+      throw new Error(`Cannot find course with this name ${courseName}`);
+    } else {
+      res.status(200).send(course.description)
+    }
   } catch (e) {
     res.status(400).send(e);
   }
@@ -134,36 +167,29 @@ app.get('/transcripts/:studentID/:course', (req, res) => {
 // helpful for debugging
 // this posts to the server console so we'll know we got here
 // replaces Express's default 404 catcher.
-app.get('/:request*', (req, res) => {
-  console.log(defaultErrorMessage('GET', req.params.request));
+app.get("/:request*", (req, res) => {
+  console.log(defaultErrorMessage("GET", req.params.request));
   res.sendStatus(404);
 });
 
-app.post('/:request*', (req, res) => {
-  console.log(defaultErrorMessage('POST', req.params.request));
+app.post("/:request*", (req, res) => {
+  console.log(defaultErrorMessage("POST", req.params.request));
   res.sendStatus(404);
 });
 
-app.delete('/:request*', (req, res) => {
-  console.log(defaultErrorMessage('DELETE', req.params.request));
+app.delete("/:request*", (req, res) => {
+  console.log(defaultErrorMessage("DELETE", req.params.request));
   res.sendStatus(404);
 });
 
-app.all('/:request*', (req, res) => {
-  console.log(defaultErrorMessage('(unknown method)', req.params.request));
+app.all("/:request*", (req, res) => {
+  console.log(defaultErrorMessage("(unknown method)", req.params.request));
   res.sendStatus(404);
 });
 
 // that should catch everything.  Otherwise express responds
 // with some html containing its own idea of what a 404 should look like.
 
-
 function defaultErrorMessage(method: string, request: string): string {
   return `unknown ${method} request "${request}"`;
 }
-
-
-
-
-
-
