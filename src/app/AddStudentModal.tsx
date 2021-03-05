8c1c65d7f98cbb67a50d2b8e75e06258622c86c8
mirandadayadkins <mirandaday16@gmail.com>
2021-03-05 15:06:41 -0500

    added delete button modal

--- src/app/AddStudentModal.tsx
+++ src/app/AddStudentModal.tsx
@@ -10,7 +10,67 @@ import {
     useDisclosure,
     useToast
 } from "@chakra-ui/react";
-import {remoteTranscriptManager} from "../client/client";
+import {deleteTranscript, remoteTranscriptManager} from "../client/client";
+import {transcriptManager} from "../types/local-transcript-manager";
+
+export const DeleteStudentModal: React.FunctionComponent<{ deleteTrandscript: (transcriptID: number)=>Promise<void> }> = ({deleteTrandscript}) => {
+    const {isOpen, onOpen, onClose} = useDisclosure()
+    const toast = useToast()
+
+    return <>
+        <Button onCLick={onOpen}>Delete Transcript</Button>
+        <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay/>
+            <ModalContent>
+                <ModalHeader>Delete a transcript</ModalHeader>
+                <ModalCloseButton/>
+                <form onSubmit={async (event) => {
+                    event.preventDefault();
+
+                    const formElements = event.target as unknown as HTMLInputElement[];
+                    const studentName = formElements[0].value;
+                    try {
+                        const studentID = await transcriptManager.getStudentIDs(studentName)
+                        const transcriptDelete = await transcriptManager.getTranscript(studentID[0]);
+                        toast({
+                            title: `Transcript ${transcriptDelete} deleted!`,
+                            isClosable:true,
+                            duration: 1500,
+                            status: "success"
+                        })
+                        await transcriptManager.getTranscript(studentID[0]);
+                        onClose();
+
+                    } catch (err) {
+                        toast({
+                            title: "An error occurred",
+                            description: "Unable to add new student",
+                            status: "error",
+                            isClosable: true,
+                            duration: 3000
+                        })
+                        console.log(err);
+                    }
+                    return false;
+                }}>
+                    <ModalBody pb={6}>
+                        <FormControl isRequired>
+                            <FormLabel>Student Name</FormLabel>
+                            <Input placeholder="Name" name="studentNaem"/>
+                        </FormControl>
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button colorScheme="blue" mr={3} type="submit">
+                            Add
+                        </Button>
+                        <Button onClick={onClose}>Cancel</Button>
+                    </ModalFooter>
+                </form>
+            </ModalContent>
+        </Modal>
+    </>
+}
 
 export const AddStudentModal: React.FunctionComponent<{ appendTranscript: (newID: number)=>Promise<void> }> = ({appendTranscript}) => {
     const {isOpen, onOpen, onClose} = useDisclosure()
